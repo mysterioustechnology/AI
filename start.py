3c8b942683621fbc748747e0d3d170a8d14ef371
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-11-30 08:38:58 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 import random
+cash = ''
+
 
 # start
 print('Hi I am a test AI made by Johnathon!')
-start = input('')
+
 
 
 # is greet?
@@ -17,6 +19,23 @@ def greet():
         greets = ["hey there!", 'sup', 'Whats Up?',
                   'How are you', 'Hellow There']
         print(greets[random])
+# is an answer to how your day is going?
+def day_answer():
+    if cash === 'greet'
+    greeting = ['hi', 'hello', 'hey']
+    intention = ""
+    if_greeting = any(greeting in start for greeting in greeting)
+    random = random.randint(0, 4)
+    if if_greeting:
+        intention = 'greating'
+    if intention == 'greating':
+        greets = ["hey there!", 'sup', 'Whats Up?',
+                  'How are you', 'Hellow There']
+        print(greets[random])
 
 
-greet()
+def input():
+    user = input('User: ')
+    cash = user;
+    greet();
+    day_answer();
