a29904bebf67df1b2c24242356292a1369883266
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-12-02 13:09:02 -0500

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,31 @@
+# is greet?
+def greet():
+    greeting = ['hi', 'hello', 'hey']
+    intention = ""
+    if_greeting = any(greeting in user for greeting in greeting)
+    random_num = random.randint(0, 4)
+    if if_greeting:
+        intention = 'greating'
+    if intention == 'greating':
+        greets = ["hey there!", 'sup', 'Whats Up?',
+                  'How are you', 'Hellow There']
+        print(greets[random])
+# is an answer to how your day is going?
+def day_answer():
+    if cash == 'greating':
+        greeting = ['hi', 'hello', 'hey']
+        intention = ""
+        if_day_answer = any(day_answer in user for day_answer in day_answer)
+        random = random.randint(0, 4)
+        if if_day_answer:
+            intention = 'greating'
+        if intention == 'greating':
+            greets = ["hey there!", 'sup', 'Whats Up?',
+                      'How are you', 'Hellow There']
+            print('Server: '+ greets[random])
+
+def user_input():
+    user = input('User: ')
+    cash = intention;
+    greet();
+    day_answer();
