987e99d353e97f3b09cc106d7cd6fad1d2f35408
mysterioustechnology <91982662+mysterioustechnology@users.noreply.github.com>
2021-12-08 21:10:49 -0500

    save

--- functions.py
+++ functions.py
@@ -2,21 +2,21 @@ import random;
 import dict;
 
 # is an answer to how your day is going?
- def day_answer(input):
+def day_answer(input):
     random_num = random.randint(0, 4)
     if dict.intention == 'day_answer':
         
         print('Server: '+ greets[random_num])
 
 
- def greet():
+def greet():
     random_num = random.randint(0, 4)
     if dict.intention == 'greating':
         x = dict.greets[random_num]
         return x
 
 
- def stop():
+def stop():
     random_num = random.randint(0, 4)
     if dict.intention == 'stop':
         x = dict.end[random_num]
